077938e4eb591e3cca929009b17b7d86a46d8c2b
abdelali-code <57986145+abdelali-code@users.noreply.github.com>
2020-02-14 14:05:10 +0100

    fix the menu of the navigation
    fiix the color of the toggle bar and the emplacement of it

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-
-let links = document.getElementById("links");
-let men = document.getElementById("menu");
-
-
-function toggleMenu() {
-    if (links.classList.contains("show")){
-        links.classList.remove("show");
-        menubar.addList.remove("hideMenu")
-    }
-    else {
-        links.classList.add("show");
-    }
-}
+document.querySelector(".menu-btn").addEventListener('click', () =>{
+    document.querySelector("#links").classList.toggle("show-menu");
+});
