0a23e86b71f69fc74e455c9f797508a0e6b1a2c5
abdelali-code <57986145+abdelali-code@users.noreply.github.com>
2020-02-16 22:59:21 +0100

    maintain the width of the container

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-document.querySelector(".menu-btn").addEventListener('click', () =>{
-    document.querySelector("#links").classList.toggle("show-menu");
-});
+// document.querySelector(".menu-btn").addEventListener('click', () =>{
+//     document.querySelector("#links").classList.toggle("show-menu");
+// });
+
+
+const menu = document.querySelector(".menu-btn");
+const links = document.querySelector("#links");
+
+menu.addEventListener("click", () =>{
+    if(!links.classList.contains("show-menu")) {
+        links.classList.add("show-menu");
+    }
+    else {
+        links.classList.remove("show-menu");
+    }
+})
