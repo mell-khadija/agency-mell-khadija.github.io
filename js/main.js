d79c1f9f2c01701e4e9a67c6a645462bf2e9e5bc
mell-khadija <57993809+mell-khadija@users.noreply.github.com>
2020-02-25 17:06:02 +0100

    final step

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+// document.querySelector(".menu-btn").addEventListener('click', () =>{
+//     document.querySelector("#links").classList.toggle("show-menu");
+// });
 
 
 const menu = document.querySelector(".menu-btn");
+const body = document.querySelector("body");
 const links = document.querySelector("#links");
 
-menu.addEventListener("click", () =>{
+body.addEventListener("click", () =>{
     if(!links.classList.contains("show-menu")) {
         links.classList.add("show-menu");
     }
