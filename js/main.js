de3f6107e48b42a8252b1cb13dfe414284ef1bbf
mell-khadija <57993809+mell-khadija@users.noreply.github.com>
2020-02-25 14:48:47 +0100

    la creation de la page de contact sans map

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ body.addEventListener("click", () =>{
         links.classList.remove("show-menu");
     }
 })
+
+
+document.getElementById("button").addEventListener("click" , function(e) { 
+   
+    var erreur;
+    var Nom = document.getElementById("Nom");
+    var Email = document.getElementById("Email");
+    var tel = document.getElementById("tel");
+    var objet = document.getElementById("objet");
+
+    if(!Nom.value){
+        erreur = "veuillez renseigner un nom";
+        document.getElementById("erreur1").innerHTML = erreur;
+    }
+    if(!Email.value){
+        erreur = "veuillez renseigner un email";
+        document.getElementById("erreur2").innerHTML = erreur;
+    }
+    if(!tel.value){
+        erreur = "veuillez renseigner un tel";
+        document.getElementById("erreur3").innerHTML = erreur;
+    }
+    if(!objet.value){
+        erreur = "veuillez renseigner un objet";
+        document.getElementById("erreur4").innerHTML = erreur;
+    }
+    if (erreur){
+        e.preventDefault();
+        document.getElementById("erreur").innerHTML = erreur;
+        return false;
+    } else {
+      alert('Formulaire envoyé !');
+    }
+})
