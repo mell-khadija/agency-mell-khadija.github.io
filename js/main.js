0b03b59ee23474a8e4d7851e40e1e846f1b48eb6
IMANE-code <57986793+IMANE-code@users.noreply.github.com>
2020-02-26 10:23:29 +0100

    save

--- js/main.js
+++ js/main.js
@@ -17,35 +17,45 @@ body.addEventListener("click", () =>{
 })
 
 
-document.getElementById("button").addEventListener("click" , function(e) { 
-   
-    var erreur;
-    var Nom = document.getElementById("Nom");
-    var Email = document.getElementById("Email");
-    var tel = document.getElementById("tel");
+function valider() {
+    var nom = document.getElementById("nom");
+    var email = document.getElementById("email");
     var objet = document.getElementById("objet");
+    var span1 = document.getElementById("span1");
+    var span2 = document.getElementById("span2");
+    var span3 = document.getElementById("span3");
+    var val = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/;
+    if (nom.value == "") {
+        nom.classList.add("erreur");
+        span1.textContent = "*Name is Required!";
+    }else{
+        nom.classList.remove("erreur");
+        nom.classList.add("valide");
+        span1.textContent = "";
+    }
 
-    if(!Nom.value){
-        erreur = "veuillez renseigner un nom";
-        document.getElementById("erreur1").innerHTML = erreur;
-    }
-    if(!Email.value){
-        erreur = "veuillez renseigner un email";
-        document.getElementById("erreur2").innerHTML = erreur;
-    }
-    if(!tel.value){
-        erreur = "veuillez renseigner un tel";
-        document.getElementById("erreur3").innerHTML = erreur;
-    }
-    if(!objet.value){
-        erreur = "veuillez renseigner un objet";
-        document.getElementById("erreur4").innerHTML = erreur;
-    }
-    if (erreur){
-        e.preventDefault();
-        document.getElementById("erreur").innerHTML = erreur;
-        return false;
+    if (email.value == "") {
+        email.classList.add("erreur");
+        span2.textContent = "*email is Required!";
     } else {
-      alert('Formulaire envoyé !');
+        if (!val.test(email.value)) {
+            email.classList.add("erreur");
+            span2.textContent = "*Forme email pas Valider !";
+            }else{
+                email.classList.remove("erreur");
+                email.classList.add("valide");
+                span2.textContent = "";
+            }
     }
-})
+        if (objet.value == "") {
+            objet.classList.add("erreur");
+            span3.textContent = "*objet is Required !";
+        }else{
+        objet.classList.remove("erreur");
+        objet.classList.add("valide");
+        span3.textContent = "";
+        }
+        
+    }
+    
+
