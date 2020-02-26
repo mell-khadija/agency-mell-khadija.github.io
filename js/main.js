1df3dfff375ade41eba6f5909b40777d982c81a1
mell-khadija <57993809+mell-khadija@users.noreply.github.com>
2020-02-26 16:23:01 +0100

    js modification

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ menu.addEventListener("click", () =>{
 
 document.getElementById("button").addEventListener("click" , function(e) { 
    
-    var erreur = '';
+    var erreur = "";
     var Nom = document.getElementById("Nom");
     var Email = document.getElementById("Email");
     var tel = document.getElementById("tel");
@@ -41,11 +41,18 @@ document.getElementById("button").addEventListener("click" , function(e) {
         erreur = "Ce champ est obligatoire";
         document.getElementById("erreur2").textContent = erreur;
     }
+    
     else if (!emailRegex.test(Email.value)) {
-        erreur = "email format pas correct";
+        erreur = "exmple@email.com";
         document.getElementById("erreur2").textContent = erreur;
     }
 
+    // first two number of the telephone number;
+    let firstTwo;
+
+    if (tel.value.length > 2) {
+        firstTwo = tel.value.slice(0, 2);
+    }
     if(!tel.value){
         erreur = "Ce champ est obligatoire";
         document.getElementById("erreur3").textContent = erreur;
@@ -54,10 +61,25 @@ document.getElementById("button").addEventListener("click" , function(e) {
         erreur = "justement des chiffre";
         document.getElementById("erreur3").textContent = erreur;       
     }
+    else if(tel.value.length !== 10) {
+        erreur = "should contains 10 digit";
+        document.getElementById("erreur3").textContent = erreur; 
+    }
+    
+    else if (!((firstTwo == "06") || (firstTwo == "05") || (firstTwo = "07") )) {
+            erreur = "phone should begin with 06 or 07 or 05";
+            document.getElementById("erreur3").textContent = erreur;
+    }
+    else {
+        erreur = "";
+        document.getElementById("erreur3").textContent = erreur;     
+    }
     if(!objet.value){
         erreur = "Ce champ est obligatoire";
         document.getElementById("erreur4").textContent = erreur;
     }
+    
+    
     if (erreur != "")
     {
         e.preventDefault();
